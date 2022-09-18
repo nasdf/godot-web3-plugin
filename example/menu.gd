7893765dd57fb629a374c8b96dd76e6bcf1fbfd6
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-09-18 13:53:59 -0700

    remove key

--- example/menu.gd
+++ example/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	var wallet = Wallet.new()
-	wallet.set_private_key("0x7c4a968dd1857caa582b50ffe89abeb11ed8cc4cc0828846b548b6307b5c6f39")
+	wallet.set_private_key("")
 	
 	$EthTransaction.connect("request_completed", self, "_request_completed")
 	$EthTransaction.request("mint", ["0x00"], wallet);
